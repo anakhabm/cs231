3a07fe197de9b4312e080b94bac0650d41e52a73
ANAKHA BM <anakhabm12@gmail.com>
2018-10-15 00:11:17 +0530

    Second commit

---
+++ seqser.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+
+int main()
+
+{
+
+    int array[10];
+
+    int i, num, keynum, found = 0;
+
+    printf("Enter the value of num \n");
+
+    scanf("%d", &num);
+
+    printf("Enter the elements one by one \n");
+
+    for (i = 0; i < num; i++)
+
+    {
+
+        scanf("%d", &array[i]);
+
+    }
+
+    printf("Input array is \n");
+
+    for (i = 0; i < num; i++)
+
+    {
+
+        printf("%dn", array[i]);
+
+    }
+
+    printf("Enter the element to be searched \n");
+
+    scanf("%d", &keynum);
+
+    for (i = 0; i < num ; i++)
+
+    {
+
+        if (keynum == array[i] )
+
+        {
+
+            found = 1;
+
+            break;
+
+        }
+
+    }
+
+    if (found == 1)
+
+        printf("Element is present in the array\n");
+
+    else
+
+        printf("Element is not present in the array\n");
+
+    return 0;
+
+}

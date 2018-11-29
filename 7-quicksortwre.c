e9015c69b2e02304499055d4fd355351bc7aeb7c
ANAKHA  BM <43615458+ceccs17d06@users.noreply.github.com>
2018-11-29 15:45:47 +0530

    Update 7-quicksortwre.c

--- 7-quicksortwre.c
+++ 7-quicksortwre.c
@@ -56,3 +56,12 @@ int main() {
 	quickSortIterative(array, 0, size-1);
 	printArray(array, size);
 }
+output:
+
+Enter the number of elements : 2
+Enter element 1 :3
+Enter element 2 :1
+Unsorted list is : 3 1 
+Size=1 
+Elements are : 1 3 Sorted list is :
+1 3

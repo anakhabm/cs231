0b9063a4329c60587437b01e0f33603b744f69fa
ANAKHA  BM <43615458+ceccs17d06@users.noreply.github.com>
2018-11-29 16:24:40 +0530

    Update 18-stackarr.c

--- 18-stackarr.c
+++ 18-stackarr.c
@@ -64,3 +64,100 @@ int main() {
 		}
 	} while (ans != '4');
 }
+	printf("\nThe popped element is %d",stack[top]);
+					top--;
+				}
+				break;
+			}
+			case 3:
+			{
+				if(top>=0)
+				{
+					printf("\nThe elements in stack \n");
+					for(i=top; i>=0; i--)
+						printf("\n%d",stack[i]);
+					printf("\nEnter Choice");
+				}
+				else
+				{
+					printf("\nThe stack is empty");
+				}
+				break;
+			}
+			case 4:
+			{
+				printf("\n\EXIT  ");
+				exit();
+			}
+			default:
+			{
+				printf ("\nPlease Enter a Valid Choice");
+			}
+
+		}
+	}while(c==1 || c==2 || c==3 || c==4);
+
+   return 0;
+}
+	printf("\nThe popped element is %d",stack[top]);
+					top--;
+				}
+				break;
+			}
+			case 3:
+			{
+				if(top>=0)
+				{
+					printf("\nThe elements in stack \n");
+					for(i=top; i>=0; i--)
+						printf("\n%d",stack[i]);
+					printf("\nEnter Choice");
+				}
+				else
+				{
+					printf("\nThe stack is empty");
+				}
+				break;
+			}
+			case 4:
+			{
+				printf("\n\EXIT  ");
+				exit();
+			}
+			default:
+			{
+				printf ("\nPlease Enter a Valid Choice");
+			}
+
+		}
+	}while(c==1 || c==2 || c==3 || c==4);
+
+   return 0;
+}
+OUTPUT:
+	Enter the size of stack:3
+
+	1.PUSH
+	2.POP
+	3.DISPLAY
+	4.EXIT
+	 Enter the Choice:1
+	Enter a value to be pushed:4
+
+	 Enter the Choice:1
+	Enter a value to be pushed:5
+
+	 Enter the Choice:1
+	Enter a value to be pushed:6
+
+	 Enter the Choice:2
+
+	The popped element is 6
+	 Enter the Choice:3
+
+	The elements in stack
+
+	5
+	4
+	 Enter the Choice:
+	5

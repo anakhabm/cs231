d2787df8df5e2a96e1a197e2d24659e59cab212f
ANAKHA  BM <43615458+ceccs17d06@users.noreply.github.com>
2018-11-29 16:41:18 +0530

    Update 31-circularqueuell.c

--- 31-circularqueuell.c
+++ 31-circularqueuell.c
@@ -236,3 +236,79 @@ break;
 }
 
 }
+OUTPUT:
+
+
+       What do you want to do??
+       1.Push
+       2.Pop
+       3.SizeOfQueue
+       4.Display
+       0.EXIT
+       1
+       What no. do you want to push in queue
+       4
+
+              What do you want to do??
+       1.Push
+       2.Pop
+       3.SizeOfQueue
+       4.Display
+       0.EXIT
+       1
+       What no. do you want to push in queue
+       6
+
+              What do you want to do??
+       1.Push
+       2.Pop
+       3.SizeOfQueue
+       4.Display
+       0.EXIT
+       1
+       What no. do you want to push in queue
+       5
+
+              What do you want to do??
+       1.Push
+       2.Pop
+       3.SizeOfQueue
+       4.Display
+       0.EXIT
+       3
+       Size of queue is 3
+
+              What do you want to do??
+       1.Push
+       2.Pop
+       3.SizeOfQueue
+       4.Display
+       0.EXIT
+       4
+       Queue is -->> 4 6 5
+
+              What do you want to do??
+       1.Push
+       2.Pop
+       3.SizeOfQueue
+       4.Display
+       0.EXIT
+       2
+       Number poped from queue is 4
+
+              What do you want to do??
+       1.Push
+       2.Pop
+       3.SizeOfQueue
+       4.Display
+       0.EXIT
+       2
+       Number poped from queue is 6
+
+              What do you want to do??
+       1.Push
+       2.Pop
+       3.SizeOfQueue
+       4.Display
+       0.EXIT
+       0
